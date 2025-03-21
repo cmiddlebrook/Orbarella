aabe4afa64785861e4dffd98fb46d402d807b485
Caroline Middlebrook <caroline.middlebrook@googlemail.com>
2025-03-21 16:23:52 +0000

    Add project files.

---
+++ Orbarella.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CALIMOE;
+
+namespace Orbarella;
+
+public class Orbarella : Calimoe
+{
+
+    public Orbarella()
+    {
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+        _showFPS = false;
+        _fallbackTextureSize = 64;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent(); // ensure the AssetManager gets loaded
+
+        var scene1 = new Scene1(_sm, _am, _ih);
+
+        _sm.AddScene(scene1);
+        _sm.SwitchScene("scene1");
+
+        _graphics.PreferredBackBufferWidth = scene1.WindowWidth;
+        _graphics.PreferredBackBufferHeight = scene1.WindowHeight;
+        _graphics.ApplyChanges();
+
+    }
+
+    protected override void Update(GameTime gt)
+    {
+        _sm.Update(gt);
+        base.Update(gt);
+    }
+
+    protected override void Draw(GameTime gt)
+    {
+        if (ClearColour != Color.Transparent)
+        {
+            GraphicsDevice.Clear(ClearColour);
+        }
+
+        _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+        _sm.Draw(_spriteBatch);
+        _spriteBatch.End();
+
+        base.Draw(gt);
+    }
+}
---
+++ Scenes/Scene1.cs
@@ -0,0 +1,42 @@
+﻿using CALIMOE;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Orbarella;
+public class Scene1 : GameScene
+{
+    private Texture2D _background;
+    public int WindowWidth => _background.Width;
+    public int WindowHeight => _background.Height;
+
+    public Scene1(SceneManager sm, AssetManager am, InputHelper ih)
+        : base(sm, am, ih)
+    {
+        _name = "scene1";
+        _clearColour = new Color(0x10, 0x10, 0x10);
+    }
+
+    public override void LoadContent()
+    {
+        _background = _am.LoadTexture("Scene1");
+    }
+
+    public override void HandleInput(GameTime gt)
+    {
+
+    }
+
+    public override void Update(GameTime gt)
+    {
+        base.Update(gt);
+        HandleInput(gt);
+    }
+
+    public override void Draw(SpriteBatch sb)
+    {
+        sb.Draw(_background, Vector2.Zero, Color.White);
+    }
+}
